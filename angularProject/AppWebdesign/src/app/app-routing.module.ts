44158ff8dc2083e54ae351db201d7d5fa1b11031
Shubheksha <priyansheadhikari@gmail.com>
2023-07-25 20:47:42 +0545

    final

--- angularProject/AppWebdesign/src/app/app-routing.module.ts
+++ angularProject/AppWebdesign/src/app/app-routing.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { from } from 'rxjs';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { DashboadComponent } from './dashboad/dashboad.component';
+
 import { FormComponent } from './form/form.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'admin', component: DashboadComponent },
+  { path: 'admin', component: AdminComponent },
   { path: 'form', component: FormComponent },
+  { path: 'form/:id', component: FormComponent },
 
 ];
 
--- angularProject/AppWebdesign/src/app/app.module.ts
+++ angularProject/AppWebdesign/src/app/app.module.ts
@@ -3,41 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
-import { DashboadComponent } from './dashboad/dashboad.component';
+import { AdminComponent } from './admin/admin.component';
 import { FormComponent } from './form/form.component';
 import { ValidationComponent } from './validation/validation.component';
-import { HighlightDirective } from './highlight.directive';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+   
     NavbarComponent,
     SidebarComponent,
     
     SignupComponent,
     LoginComponent,
     
-    DashboadComponent,
+    AdminComponent, 
     FormComponent,
-    ValidationComponent,
-    HighlightDirective,   
+    ValidationComponent,    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    
     ReactiveFormsModule,
     FormsModule
   ],
